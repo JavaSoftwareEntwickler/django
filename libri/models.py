e67e145e5f50063d2f6b2864fa80e388f4f3445f
JavaSoftwareEntwickler <mm.java.software@gmail.com>
2023-06-17 10:35:11 +0200

    firs commit

---
+++ libri/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Libro(models.Model):
+    immagine = models.ImageField()
+    titolo = models.CharField(max_length=30)
+    prezzo = models.FloatField()
+
+    def __str__(self):
+        return self.titolo
